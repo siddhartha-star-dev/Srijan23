ff53844ce27302c6d3e2e9e5e22465b74e654f9e
siddhartha-star-dev <bestsid1@gmail.com>
2023-01-18 17:29:14 +0530

    Sponsor changes

--- srijan_backend/apps/main/models/marketing.py
+++ srijan_backend/apps/main/models/marketing.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class SponsorType(models.TextChoices):
+    TITLE = "title", _("Title")
+    DECORATION = "decoration", _("Decoration")
+    ASSOCIATE = "associate", _("Associate")
+    STRATEGIC = "strategic", _("Strategic")
+    PRIZE = "prize", _("Prize")
+    LOGISTICS = "logistics", _("Logistics")
+    STYLE = "style", _("Style")
+    HEALTH = "health", _("Health")
+    BEVERAGE = "beverage", _("Beverage")
+    ONLINE = "online", _("Online")
 
 
 class Sponsor(models.Model):
@@ -6,6 +20,7 @@ class Sponsor(models.Model):
     name = models.CharField(max_length=100)
     logo = models.ImageField(upload_to="sponsor_logos")
     url = models.URLField()
+    sponsor_type = models.CharField(choices=SponsorType.choices, max_length=20)
 
     def __str__(self):
         return self.name
